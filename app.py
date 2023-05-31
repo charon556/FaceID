faf138e4ec833beeb3451c5a99bf7f37f457bdcf
charon556 <119225162+charon556@users.noreply.github.com>
2023-05-31 17:55:44 +0800

    Add comment

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import tkinter as tk
 import os
 import subprocess
 
+# 執行 inputFace.py 腳本的函數
 def run_inputFace():
     subprocess.call(["python", "inputFace.py"])
 
+# 執行 model.py 腳本的函數
 def run_model():
     subprocess.call(["python", "model.py"])
 
+# 執行 faceID.py 腳本的函數
 def run_faceID():
     subprocess.call(["python", "faceID.py"])
 
-# 建立視窗
+# 建立一個視窗
 window = tk.Tk()
-window.title("程式功能")
+window.title("程式功能")  # 設定視窗標題
 
 # 建立按鈕並設定其命令
-inputFace_button = tk.Button(window, text="輸入人臉", command=run_inputFace)
-model_button = tk.Button(window, text="訓練模型", command=run_model)
-faceID_button = tk.Button(window, text="人臉辨識", command=run_faceID)
+inputFace_button = tk.Button(window, text="輸入人臉", command=run_inputFace)  # 按鈕，執行 inputFace.py
+model_button = tk.Button(window, text="訓練模型", command=run_model)  # 按鈕，執行 model.py
+faceID_button = tk.Button(window, text="人臉辨識", command=run_faceID)  # 按鈕，執行 faceID.py
 
 # 放置按鈕到視窗中
 inputFace_button.pack()
--- inputFace.py
+++ inputFace.py
@@ -15,26 +15,34 @@ folder_path = os.path.join('data_dir', name)
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
+# 載入人臉分類器
 classfier = cv2.CascadeClassifier(cv2.data.haarcascades +"haarcascade_frontalface_alt2.xml")
 while(True):
+    # 從攝像頭讀取畫面
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faceRects = classfier.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 3, minSize = (32, 32))
+    # 在灰度圖像中檢測人臉
+    faceRects = classfier.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3, minSize=(32, 32))
 
-    if len(faceRects) > 0:      
+    if len(faceRects) > 0:
+        # 如果檢測到人臉
         for (x, y, w, h) in faceRects:
+            # 提取人臉區域
             face = frame[y - 10: y + h + 10, x - 10: x + w + 10]
-            cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), (0,255,0), 2)
+            # 在原始畫面上畫出人臉矩形框
+            cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), (0, 255, 0), 2)
+            # 生成一個隨機數字作為圖像檔案名稱
             random_number = random.randint(1000000000, 9999999999)
-            # 在偵測到人臉時，保存圖像到相應的資料夾中
+            # 在檢測到人臉時，將圖像保存到對應的資料夾中
             cv2.imwrite(f'data_dir/{name}/{str(random_number)}.jpg', face)
+    
+    # 顯示攝像頭畫面
+    cv2.imshow('live', frame)
     
     if cv2.waitKey(1) == ord('q'):
         break
-        
-    cv2.imshow('live', frame)
 
-
+# 釋放攝像頭資源並關閉視窗
 cap.release()
 cv2.destroyAllWindows()
